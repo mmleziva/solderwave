eba1a232add740d8442c0441109a8bb5940d8b9a
mmleziva <31937502+mmleziva@users.noreply.github.com>
2023-10-13 22:23:41 +0200

    Add files via upload

--- solderwave.c
+++ solderwave.c
@@ -8,6 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <xc.h>
+#pragma config FOSC = XT
+#pragma config LVP = ON
+#pragma config PWRTE = OFF
+#pragma config BOREN = ON
+#pragma config WDTE = ON
+
+#define DelayIni T1CON=0b000000000;
+#define delay(x) {TMR1= ~(x); tim1();}
+#define DelayUs(x) delay(x)
+#define DelayMs(x) delay(1000*x)
+void tim1(void)
+{TMR1IF=0;	
+ TMR1ON=1;
+ while(!TMR1IF);
+ TMR1ON=0;	
+}
+
  // outputs
 #define CERPADLO    RC0
 #define PWM_1kHz    RC1	
@@ -18,6 +35,12 @@
 #define LEDM        RA4
 #define LEDR        RA5
 
+//analog input channels
+#define TPREC        0
+#define TCHLC        1
+#define TRYCHC       2
+
+#define MULT       (6*(60/4)*(1000/8)/(0x400/(4*8)))
 //filtered inputs
 union
 {
@@ -33,9 +56,26 @@ union
       uint8_t       : 1; 
       uint8_t RESTART: 1; 
     };
-} in;
+} ai,  fil, fh,fd;
 
-uint8_t step;
+uint8_t in[8], set, res, film ,aux;
+
+uint8_t step, k, j ;
+
+uint16_t TpreAD,TchlAD;
+
+uint32_t Tmils,Tpre,Tchl;
+
+uint16_t adc_read(unsigned char channel)
+{  
+ ADCON0 = (channel << 3) | 0xC1;		// !T enable ADC, RC osc.
+ DelayUs(8); 
+ GO_DONE = 1;
+ while(GO_DONE)
+ continue;	// wait for conversion complete
+ return (((uint16_t)ADRESH)<<8) + ADRESL;
+}
+
 int main(int argc, char** argv)
 {
    TRISC=0x80;     //outs enable
@@ -51,19 +91,134 @@ int main(int argc, char** argv)
    PR2=250;
    T2CONbits.TMR2ON= 1;//start T2
    
-   step=0;
+   //step=0;
+   step=52;//t
+   //ini filters
+   ai.B= ~PORTB;
+   ai.RESTART= !RC7;
+   film= ai.B; //inverted inputs
+   for (j=0; j<8; j++)
+       {
+           in[j]= film;
+        }
+        fil.B= film;
+   fd.B=0;
+   fh.B=0;
    while(1)
    {    
+     CLRWDT();
      if(TMR2IF)//1ms
      {
        TMR2IF=0;  
+       k++;
+       k %=8;
+                //filters
+       ai.B= ~PORTB;    //inverted inputs
+       ai.RESTART= !RC7;
+       in[k]= ai.B;
+       set=0xff;
+       res=0;
+       for (j=0; j<8; j++)
+       {
+           set &= in[j];
+           res |= in[j];
+       }
+       fil.B= ((~film) & set) | (film & (~res));
+       fd.B= fd.B |(film & (~fil.B) );//fall edge        
+       fh.B= fh.B |((~film) & fil.B ); //rise edge 
+       film= fil.B;// memory
+       
        switch (step)
        {
-        case 0:
+        case 10://ini
+            if(fh.VREADY)//vozik najety
+            {
+             fh.VREADY=0;
+             step=20;
+            }
+            break;
+        case 20: //ready
+            if(fil.VREADY & fh.START)
+            {
+                fh.START=0;
+                fd.VREADY=0;
+                POJEZD= 1;
+                step=30;
+            }
+            break;
+        case 30: //arrive
+            if(fd.VREADY )
+            {
+                fd.VREADY=0;
+                step=40;
+                FLUX= 1;
+            }
+            break;
+        case 40: //flux
+            if(fh.EFLUXIN )
+            {
+                fh.EFLUXIN=0;
+                FLUX=0;
+                step=50;
+            }
+            break;
+        case 50: //go to preheating
+            if(fh.PREHIN )
+            {
+                fh.PREHIN=0;
+                POJEZD=0;
+                Tmils=0;
+                step=52;
+            }
+            break;
+        case 52: //preheating
+            TpreAD= adc_read(TPREC);
+            Tpre= MULT* (uint32_t)TpreAD;
+            if(Tmils >= Tpre)
+            {
+                POJEZD=1;
+                step=60;
+            }
+            else
+                Tmils++;
+            break;
+        case 60: //go to wave
+            if(!fil.PREHIN)
+            {
+                CERPADLO=1;
+                step=70;
+            }
+            break;
+        case 70: //wave pump
+            if(fil.CHLAZIN)
+            {
+                CERPADLO=0;
+                POJEZD=0;
+                CHLAZENI=1;
+                Tmils=0;
+                step=80;
+            }
+            break;
+        case 80: //cooling
+            TchlAD= adc_read(TCHLC);
+            Tchl= MULT* (uint32_t)TchlAD;
+            if(Tmils >= Tchl)
+            {
+                CHLAZENI=0;
+                step=90;
+            }
+            else
+                Tmils++;
+            break;
+        case 90://finish
+            if(!fil.CHLAZIN)//vozik odjety
+            {
+             step=10;
+            }
             break;
         default:
             break;
-       }
+       }     
      }
    }
     return (EXIT_SUCCESS);
